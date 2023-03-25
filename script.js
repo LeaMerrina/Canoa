ea406d178b75ca2d558e99f62f0009a1e88249dd
LeaMerrina <merrina.lea@hotmail.fr>
2023-03-25 11:08:55 -0900

    jeu sur la nav

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function SeeMore() {
 };
 
 
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
 
@@ -10,13 +14,35 @@ document.addEventListener("DOMContentLoaded", function () {
     SeeMore();
   };
 
+  let tt = document.querySelector("header").offsetHeight + "px";
   setTimeout(() => {
-      let tt = document.querySelector("header").offsetHeight + "px";
       document.documentElement.style.setProperty("--navHeight", tt);
   }, "1000");
 
-  
+  let header = document.getElementById("header");
+  let lastScrollValue = 0;
 
+  
+  document.addEventListener('scroll',() => {
+		let top  = document.documentElement.scrollTop;
+    if(lastScrollValue < top) {
+    	header.classList.add("hidden");
+    } else {
+    	header.classList.remove("hidden");
+    }
+    lastScrollValue = top;
+  });
+
+  document.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 550) {
+      header.classList.add("mini")
+    } else {
+      header.classList.remove("mini")
+    }
+    tt = '120px'
+  })
+
+  console.log(window.scrollY);
 
 
 });
