92aaa12fc3b064a309817cae1b231dd399911dfa
LeaMerrina <merrina.lea@hotmail.fr>
2023-04-07 09:16:12 +1300

    shop en cours

--- script.js
+++ script.js
@@ -2,6 +2,43 @@ function SeeMore() {
     document.querySelector("header").classList.toggle("openNav");
 };
 
+function addZero(i) {
+  if (i < 10) {
+      i = "0" + i
+  }
+  return i;
+};
+
+const date = new Date();
+let hours = addZero(date.getHours());
+let minutes = addZero(date.getMinutes());
+let seconds = addZero(date.getSeconds())
+let time = `${hours}:${minutes}:${seconds}`;
+
+let img;
+
+let mode = '';
+
+const params = [
+  {
+      image: 'sunset',
+      start: '17:00:00',
+      end: '20:59:59'
+  },
+  {
+      image: 'night',
+      start: '21:00:00',
+      mode: 'blanc',
+      end: '23:59:59'
+  },
+  {
+      image: 'night',
+      start: '00:00:00',
+      mode: 'blanc',
+      end: '07:59:59'
+  } 
+];
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -15,6 +52,40 @@ document.addEventListener("DOMContentLoaded", function () {
       document.documentElement.style.setProperty("--navHeight", tt);
   }, "1000");
 
+  let heightHero = document.getElementById('header').offsetHeight;
+
+  function displayMode(mode){
+    document.querySelector('body').classList.toggle(mode);
+};
+
+  function displayImage(img) {
+      const picture = document.getElementById('picture');
+      picture.innerHTML = `
+        <picture>
+          <source srcset="assets/photos/${img}-small.jpg" media="(max-width: 700px)">
+          <img src="assets/photos/${img}-default.jpg" alt="${img}">
+        </picture>
+      `;
+  };
+
+  for (let index = 0; index < params.length; index++) {
+      const element = params[index];
+      const startTime = new Date(`01/01/2023 ${element.start}`);
+      const endTime = new Date(`01/01/2023 ${element.end}`);
+      const currentTime = new Date(`01/01/2023 ${time}`);
+      if(currentTime >= startTime && currentTime <= endTime){
+          img = element.image;
+          mode = element.mode;
+          displayMode(mode);
+          displayImage(img);
+          break;
+      }
+  };
+
+  if(!img){
+      img = 'day'
+  };
+
   let header = document.getElementById("header");
   let lastScrollValue = 0; 
 
@@ -27,12 +98,22 @@ document.addEventListener("DOMContentLoaded", function () {
     	header.classList.remove("hidden");
     }
     lastScrollValue = top;
+    if (window.scrollY > heightHero) {
+      document.getElementById('header').classList.add('small')
+    } else {
+      document.getElementById('header').classList.remove('small')
+    }   
+    if (img === 'night' && window.scrollY > heightHero) {
+      document.getElementById('header').classList.add('night')
+    } else {
+      document.getElementById('header').classList.remove('night')
+    }
   });
 
   const itemsGrid = Array.from(document.getElementsByClassName('bloc-img'));
 
   itemsGrid.forEach(element => { 
-    const r = gsap.utils.random(-30, 30)
+    const r = gsap.utils.random(-10, 10)
     gsap.set(element, {rotation: r})
   });
 
@@ -51,8 +132,20 @@ document.addEventListener("DOMContentLoaded", function () {
       root.style.setProperty('--scroll-y', scroll);
   });
 
+  const txtShop = Array.from(document.getElementsByClassName('txt-shop'));
+  let currentElement;
+
+  txtShop.forEach(element => {
+    element.addEventListener('mouseover', (e) => {
+      if (currentElement) {
+        gsap.to(currentElement, 0.2, {opacity: 0, overwrite: true});
+      }
+      let myId = e.currentTarget.getAttribute('id');
+      gsap.to(`.${myId}`, 0.2, {opacity: 1, overwrite: true});
+      currentElement = `.${myId}`;
+      
+    })
+  })
 
 });
-
-
 
